da4e7dd726fb283e0e5523aa198ea45bcab2fef5
WANG Longqi <iqgnol@gmail.com>
2015-03-24 20:33:38 +0800

    read data in

---
+++ importdata.py
@@ -0,0 +1,19 @@
+# read in data
+import pandas as pd
+
+def read_train():
+	train = pd.read_csv('train.csv')
+	toint = lambda x:int(x[-1:])
+	y = map(toint,train.target)
+	train.drop('target', axis=1, inplace=True)
+	train.drop('id', axis=1, inplace=True)
+	X = train.values
+	y = array(y)
+	return (X,y)
+
+
+def read_test():
+	test = pd.read_csv('test.csv')
+	test.drop('id', axis=1, inplace=True)
+	test = test.values
+	return test
